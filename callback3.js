8352748bead871ea564a3182f8d075c6e7261a77
albertteal <albertteal@gmail.com>
2023-11-21 16:39:40 -0600

    se modifica el 3er programa de callback3

--- callback3.js
+++ callback3.js
@@ -2,15 +2,16 @@ function hola(nombre, miCallback){
     setTimeout(function ()  {
         console.log('Hola, ' + nombre);
         miCallback();
-    }, 1000);
+    }, 2000);
 }
 
 function adios(nombre, otroCallback){
     setTimeout(function ()  {
         console.log('adios, ' +  nombre);
         otroCallback();
-    }, 1000);
+    }, 2000);
 }
+
 console.log('Inciando proceso...');
 hola('Albert', function(){
     adios('Albert', function(){
